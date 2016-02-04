b4ed889dad26aff9ad7abae3a5c64ed5a4422347
joneland <jonathan.eland@gmail.com>
2016-02-04 20:07:48 +0000

    Setting up project for all future hands on sessions

---
+++ src/main/java/elandjo/sandbox/tdd/Placeholder.java
@@ -0,0 +1,5 @@
+package elandjo.sandbox.tdd;
+
+public class Placeholder {
+    // Placeholder to be able to commit to git
+}
---
+++ src/test/java/elandjo/sandbox/tdd/PlaceholderTest.java
@@ -0,0 +1,5 @@
+package elandjo.sandbox.tdd;
+
+public class PlaceholderTest {
+    // Placeholder to be able to commit to git
+}
